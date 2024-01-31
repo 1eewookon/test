ee847367a7c7a8d6c0edf918221e7665e44f0206
phw0007 <yp112.nmnfnasd@gmail.com>
2024-01-31 13:09:00 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+    //수업목표  오늘의 수업 목표입니다.
+
+    // 여기는 주석입니다.
+    /*여기는
+    * 여러 줄
+    * 주석 입니다.
+    * */
+
+    /*
+    * ctrl - space : 자동완성 기능
+    * ctrl - d
+    * */
+
+    //복사 예시
+    //복사 예시
+    //복사 예시
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다.");
+        System.out.println("저는 이우곤 입니다.");
+    }
+}
